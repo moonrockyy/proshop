cbe037782967d671fa70864e789aa5fd4a48e90c
Mahan Jafari <mahanjafari14@gmail.com>
2023-12-15 23:09:56 +0330

    presentation customization

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -10,8 +10,7 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      // NOTE: we don't need user, rating, numReviews or reviews
-      // in the cart
+
       const { user, rating, numReviews, reviews, ...item } = action.payload;
 
       const existItem = state.cartItems.find((x) => x._id === item._id);
@@ -42,8 +41,7 @@ const cartSlice = createSlice({
       state.cartItems = [];
       localStorage.setItem('cart', JSON.stringify(state));
     },
-    // NOTE: here we need to reset state for when a user logs out so the next
-    // user doesn't inherit the previous users cart and shipping
+
     resetCart: (state) => (state = initialState),
   },
 });
